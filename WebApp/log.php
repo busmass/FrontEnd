02b1082c8ec793b9983831119e544b3e3e9060d6
youri <youri@NetBSD.org>
2015-10-18 09:34:01 +0200

    add log

---
+++ WebApp/log.php
@@ -0,0 +1,12 @@
+<div class="container">
+   <div class="logoForLogin">
+   <img class="img-responsive" src="img/busmass.png" alt="Logo" width="80%" height="40%" />
+   </div>
+  </div>
+  <div class="col-sm-10">
+    <input type="text" class="form-control" id="pwd" placeholder="Email or username">
+    <input type="password" class="form-control" id="pwd" placeholder="Password">
+        <div class="buttonForget">
+          <button type="button" class="btn btn-link"><h3>Forgot your password ?</h3></button>
+    </div>
+</div>
